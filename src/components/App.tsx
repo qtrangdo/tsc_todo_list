46804b3626ac1d1dfcd3dae78d466fb12ceb4332
td94 <quynhtrangdo94@gmail.com>
2019-05-15 15:42:27 -0400

    Add form and handle submit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react';
 
-class App extends React.Component<any, any> {
+class App extends React.Component<{}, {}> {
+  handleSubmit(event: any) {
+    event.preventDefault(),
+    console.log("submit")
+  }
+
   render() {
     return (
-      <h1>Hello World!!!</h1>
+      <div>
+        <h1>React Typescript Todo List</h1>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input type="text" placeholder="Type new Task"/>
+          <button type="submit">Add Tasks</button>
+        </form>
+      </div>
     )
   }
 }
