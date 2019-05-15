fb67481e2ecfec0ce94c233f1edce95d2cae87dd
td94 <quynhtrangdo94@gmail.com>
2019-05-15 16:24:59 -0400

    Handdle App state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,48 @@
 import * as React from 'react';
 
-class App extends React.Component<{}, {}> {
-  handleSubmit(event: any) {
+interface IAppState {
+  currentTask: string,
+  tasks: Array<string>
+}
+
+class App extends React.Component<{}, IAppState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      currentTask : '',
+      tasks : []
+    }
+  }
+
+  handleSubmit(event: any):void {
     event.preventDefault(),
-    console.log("submit")
+    this.setState({
+      tasks: [
+        ...this.state.tasks,
+        this.state.currentTask
+      ],
+      currentTask : ''
+    })
+  }
+
+  onChange(event: any):void {
+    const currentTask = event.target.value;
+    this.setState({ currentTask })
   }
 
   render() {
     return (
       <div>
         <h1>React Typescript Todo List</h1>
-        <form onSubmit={this.handleSubmit.bind(this)}>
-          <input type="text" placeholder="Type new Task"/>
+        <form 
+          onSubmit={this.handleSubmit.bind(this)}
+          >
+          <input 
+            type="text" 
+            placeholder="Type new Task"
+            value={this.state.currentTask}
+            onChange={this.onChange.bind(this)}
+          />
           <button type="submit">Add Tasks</button>
         </form>
       </div>
