46550b502389bfbcbe5071c5805b7c65d86be94d
td94 <quynhtrangdo94@gmail.com>
2019-05-15 16:44:15 -0400

    Add types to all functions

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -15,7 +15,7 @@ class App extends React.Component<{}, IAppState> {
     }
   }
 
-  handleSubmit(event: any):void {
+  public handleSubmit(event: React.FormEvent<HTMLFormElement>):void {
     event.preventDefault(),
     this.setState({
       tasks: [
@@ -26,18 +26,18 @@ class App extends React.Component<{}, IAppState> {
     })
   }
 
-  onChange(event: any):void {
-    const currentTask = event.target.value;
+  public onChange(event: React.FormEvent<HTMLInputElement>):void {
+    const currentTask = event.currentTarget.value;
     this.setState({ currentTask })
   }
 
-  renderTasks() {
+  public renderTasks(): JSX.Element[] {
     return this.state.tasks.map((task: string, index: number) => (
       <div key={index}>{task}</div>
     ))
   }
 
-  render() {
+  public render(): JSX.Element {
     return (
       <div>
         <h1>React Typescript Todo List</h1>
